d20a29cedabf9820c18581971170d6b793cea296
Dennis Buijs <dennisdennisbuijs@gmail.com>
2024-12-08 17:04:36 +0100

    update day 6, part 1

--- day-6/main.go
+++ day-6/main.go
@@ -1,145 +1,112 @@
 package main
 
 import (
+	"aoc24/util"
 	"bufio"
 	"fmt"
-	"log"
 	"os"
+	"os/exec"
 )
 
+type Grid struct {
+	cells [][]string
+}
+
 type Guard struct {
-	x, y      int
+	position  Position
 	direction string
 }
 
+type Position struct {
+	x, y int
+}
+
+type Vector struct {
+	x, y int
+}
+
+var grid Grid
+var blocks []Position
+var guard Guard
+
+var autoStep = true
+
+var directionVectors = map[string]Vector{
+	"UP":    {x: 0, y: -1},
+	"DOWN":  {x: 0, y: 1},
+	"LEFT":  {x: -1, y: 0},
+	"RIGHT": {x: 1, y: 0},
+}
+
+var directionCharacters = map[string]string{
+	"UP":    "^",
+	"DOWN":  "v",
+	"LEFT":  "<",
+	"RIGHT": ">",
+}
+
+func (g Guard) cacheKey() string {
+	return fmt.Sprintf("%v-%v", g.position.x, g.position.y)
+}
+
+var guardHistory = map[string]bool{}
+
 func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	grid = Grid{util.OpenFileAsStringGrid("test.txt")}
+	guard := Guard{direction: "UP"}
 
-	defer file.Close()
+	for y := 0; y < len(grid.cells); y++ {
+		for x := 0; x < len(grid.cells[0]); x++ {
+			cell := grid.cells[y][x]
 
-	gridHeight := 100
-
-	grid := make([][]string, 0, gridHeight)
-	var startX, startY int
-	y := 0
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := make([]string, len(scanner.Text()))
-		for i, char := range scanner.Text() {
-			line[i] = string(char)
-			if line[i] == "^" {
-				startX = i
-				startY = y
+			if cell == "#" {
+				blocks = append(blocks, Position{x: x, y: y})
 			}
-		}
-		grid = append(grid, line)
-		y++
-	}
 
-	guard := Guard{x: startX, y: startY, direction: "UP"}
-	positionsVisited := [][]int{}
-
-	for !outOfBounds(grid, guard) {
-		if guard.direction == "UP" {
-			if nextPositionIsAvailable(grid, guard.x, guard.y-1) {
-				guard.y--
-				positionsVisited = append(positionsVisited, []int{guard.x, guard.y})
-				continue
-			} else {
-				guard.turnRight()
-				continue
-			}
-		}
-
-		if guard.direction == "RIGHT" {
-			if nextPositionIsAvailable(grid, guard.x+1, guard.y) {
-				guard.x++
-				positionsVisited = append(positionsVisited, []int{guard.x, guard.y})
-				continue
-			} else {
-				guard.turnRight()
-				continue
-			}
-		}
-
-		if guard.direction == "DOWN" {
-			if nextPositionIsAvailable(grid, guard.x, guard.y+1) {
-				guard.y++
-				positionsVisited = append(positionsVisited, []int{guard.x, guard.y})
-				continue
-			} else {
-				guard.turnRight()
-				continue
-			}
-		}
-
-		if guard.direction == "LEFT" {
-			if nextPositionIsAvailable(grid, guard.x-1, guard.y) {
-				guard.x--
-				positionsVisited = append(positionsVisited, []int{guard.x, guard.y})
-				continue
-			} else {
-				guard.turnRight()
-				continue
+			if cell == "^" {
+				guard.position = Position{x: x, y: y}
 			}
 		}
 	}
 
-	uniquePositions := [][]int{}
+	grid.cells[guard.position.y][guard.position.x] = "."
 
-	for _, position := range positionsVisited {
-		notUnique := false
-		for _, uniquePosition := range uniquePositions {
-			if position[0] == uniquePosition[0] && position[1] == uniquePosition[1] {
-				notUnique = true
-				break
-			}
+	scanner := bufio.NewScanner(os.Stdin)
+	for true {
+		clearTerminal()
+		grid.draw(guard)
+		fmt.Printf("[GUARD X:%v Y:%v]", guard.position.x, guard.position.y)
+		fmt.Printf("[GUARD DIR:%s]", guard.direction)
+		fmt.Printf("[GUARD STEPS:%v]", len(guardHistory))
+
+		if guard.outOfBounds() {
+			clearTerminal()
+			fmt.Printf("Guard out of bounds after %v steps\n", len(guardHistory))
+			break
+		} else {
+			guardHistory[guard.cacheKey()] = true
+			guard.step()
 		}
 
-		if !notUnique {
-			uniquePositions = append(uniquePositions, position)
+		if !autoStep {
+			scanner.Scan()
+			scanner.Text()
 		}
 	}
-
-	fmt.Println(len(uniquePositions))
 }
 
-func outOfBounds(grid [][]string, guard Guard) bool {
-	if guard.direction == "UP" {
-		return guard.y == 0
+func (g *Guard) step() {
+	if g.nextStepIsBlocked() {
+		g.turnRight()
+		return
 	}
 
-	if guard.direction == "RIGHT" {
-		return guard.x == len(grid[guard.y])
-	}
-
-	if guard.direction == "DOWN" {
-		return guard.y == len(grid)
-	}
-
-	if guard.direction == "LEFT" {
-		return guard.x == 0
-	}
-
-	return false
-}
-
-func nextPositionIsAvailable(grid [][]string, x int, y int) bool {
-	return grid[y][x] != "#"
+	g.position = g.position.applyVector(directionVectors[g.direction])
 }
 
 func (g *Guard) turnRight() {
 	if g.direction == "UP" {
 		g.direction = "RIGHT"
-		return
-	}
-
-	if g.direction == "RIGHT" {
-		g.direction = "DOWN"
 		return
 	}
 
@@ -152,4 +119,48 @@ func (g *Guard) turnRight() {
 		g.direction = "UP"
 		return
 	}
+
+	if g.direction == "RIGHT" {
+		g.direction = "DOWN"
+		return
+	}
+}
+
+func (g Guard) nextStepIsBlocked() bool {
+	nextPosition := g.position.applyVector(directionVectors[g.direction])
+
+	for _, b := range blocks {
+		if b == nextPosition {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (p Position) applyVector(v Vector) Position {
+	return Position{x: p.x + v.x, y: p.y + v.y}
+}
+
+func (g Grid) draw(guard Guard) {
+	for y, row := range g.cells {
+		for x, cell := range row {
+			if guard.position.x == x && guard.position.y == y {
+				fmt.Print(directionCharacters[guard.direction])
+			} else {
+				fmt.Print(cell)
+			}
+		}
+		fmt.Print("\n")
+	}
+}
+
+func (g Guard) outOfBounds() bool {
+	return g.position.x < 0 || g.position.x == len(grid.cells[0]) || g.position.y < 0 || g.position.y == len(grid.cells)
+}
+
+func clearTerminal() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
---
+++ util/util.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func OpenFileAsStringGrid(path string) [][]string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grid := [][]string{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		row := make([]string, len(line))
+		for col, r := range line {
+			row[col] = string(r)
+		}
+		grid = append(grid, row)
+	}
+
+	return grid
+}
